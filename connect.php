d1b1172053611d60ec044e692b8c9481c7c3c119
Vidhi <vidhi.gandhi1405@gmail.com>
2024-12-14 19:17:02 +0530

    new

---
+++ connect.php
@@ -0,0 +1,19 @@
+<?php
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+    $name=$_POST['name'];
+    $gender=$_POST['gender'];
+    $age=$_POST['age'];
+    $Email=$_POST['Email'];
+    $contactnumber=$_POST['contactnumber'];
+    $country=$_POST['country'];
+    $Cardtype=$_POST['Cardtype'];
+    $skills=$_POST['skills'];
+    $experience=$_POST['experience'];
+    $companyname=$_POST['companyname'];
+    $role=$_POST['role'];
+    $duration=$_POST['duration'];
+    $projects=$_POST['projects'];
+    //database connection
+
+?>
